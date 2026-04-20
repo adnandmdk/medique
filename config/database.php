4bffe63b3944d4bedccb7c0ad9d0a5782ce141de
adnandmdk <adnandmdk@gmail.com>
2026-04-20 10:06:54 +0700

    fix db host railway

--- config/database.php
+++ config/database.php
@@ -47,7 +47,7 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $_ENV['DB_HOST'] ?? '127.0.0.1',
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'medique'),
             'username' => env('DB_USERNAME', 'root'),
